65770344ab5a16dd831229ae40533d0bd71beeae
Usha Kollu <ushakollu@yahoo.com>
2026-03-21 09:04:04 -0700

    updated azure setup

--- locustfile.py
+++ locustfile.py
@@ -1,19 +1,10 @@
-import sys
-import os
-# This must stay at the top
-sys.path.append(os.getcwd())
+from locust import between
+from tests.get_AllBookingIds import GetAllBookingIds
+from utils.base import BaseUser
 
-from locust import between  
-
-try:
-    from tests.get_AllBookingIds import GetAllBookingIds
-    from utils.config_loader import ConfigLoader
-    from utils.base import BaseUser # <--- IMPORT YOUR BASE CLASS HERE
-except ImportError:
-    from get_AllBookingIds import GetAllBookingIds
-    from config_loader import ConfigLoader
-    from base import BaseUser # <--- FALLBACK FOR FLATTENED FILES
 
 class MyTests(BaseUser):
     wait_time = between(1, 5)
     tasks = [GetAllBookingIds]
+
+
