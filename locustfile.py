b3ac87ac55b2cf7730bc304dbf7ab2b496f32e5e
Usha Kollu <ushakollu@yahoo.com>
2026-03-21 01:14:35 -0700

    flat vs non flat

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,17 @@
-from locust import between
-from tests.get_AllBookingIds import GetAllBookingIds
-from utils.base import BaseUser
+import sys
+import os
+
+# This line is mandatory for Azure to 'see' the uploaded files
+sys.path.append(os.getcwd())
+
+try:
+    # 1. Try importing via the folder (Standard way)
+    from tests.get_AllBookingIds import GetAllBookingIds
+    from utils.config_loader import ConfigLoader
+except ImportError:
+    # 2. Try importing directly (The 'Flattened' way Azure often uses)
+    from get_AllBookingIds import GetAllBookingIds
+    from config_loader import ConfigLoader
 
 class MyTests(BaseUser):
     wait_time = between(1, 5)
